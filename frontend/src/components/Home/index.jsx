6e4e331bb243e8fe446b2e63d38707dc5984a6ab
Richardson William <richardsonwtr@gmail.com>
2022-03-26 20:36:09 -0300

    refactor: Simplifying the Home component layout

--- frontend/src/components/Home/index.jsx
+++ frontend/src/components/Home/index.jsx
@@ -1,86 +1,84 @@
 import React from "react";
-import { Container, Grid } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import Folder from "assets/folder.png";
 import MedicalResult from "assets/medical-result.png";
 import PlaceholderRotated from "assets/placeholder-rotated.png";
 import Sheet from "assets/sheet.png";
-import IconButton from 'components/IconButton'
+import IconButton from "components/IconButton";
 
 const Home = () => {
   return (
-    <Container style={{ padding: 0, height: "100vh" }}>
-      <Grid container direction="row" style={{ padding: 0, height: "100vh" }}>
+    <Grid container direction="row" style={{ height: "100%" }}>
+      <Grid
+        item
+        style={{ flexGrow: "1", height: "80vh" }}
+        container
+        direction="column"
+      >
         <Grid
           item
-          style={{ flexGrow: "1", height: "80vh" }}
+          style={{ flexGrow: "1" }}
           container
-          direction="column"
+          justifyContent="center"
+          alignItems="center"
         >
           <Grid
-            item
-            style={{ flexGrow: "1" }}
             container
-            justifyContent="center"
             alignItems="center"
+            justifyContent="center"
+            spacing={0}
+            style={{ maxHeight: "40vh" }}
           >
             <Grid
+              item
+              style={{ padding: 20 }}
+              xs={6}
               container
               alignItems="center"
               justifyContent="center"
-              spacing={0}
-              style={{ maxHeight: "40vh" }}
             >
-              <Grid
-                item
-                style={{ padding: 20 }}
-                xs={6}
-                container
-                alignItems="center"
-                justifyContent="center"
-              >
-                <IconButton
-                  imageUrl={Folder}
-                  name={"Histórico de Atendimento"}
-                />
-              </Grid>
-              <Grid
-                item
-                style={{ padding: 20 }}
-                xs={6}
-                container
-                alignItems="center"
-                justifyContent="center"
-              >
-                <IconButton imageUrl={PlaceholderRotated} name={"Exames Realizados"} />
-              </Grid>
-              <Grid
-                item
-                style={{ padding: 20 }}
-                xs={6}
-                container
-                alignItems="center"
-                justifyContent="center"
-              >
-                <IconButton imageUrl={Sheet} name={"Hábitos & Saúde"} />
-              </Grid>
-              <Grid
-                item
-                style={{ padding: 20 }}
-                xs={6}
-                container
-                alignItems="center"
-                justifyContent="center"
-              >
-                <IconButton
-                  imageUrl={MedicalResult}
-                  name={"Questionário de Emergência"}
-                />
-              </Grid>
+              <IconButton imageUrl={Folder} name={"Histórico de Atendimento"} />
+            </Grid>
+            <Grid
+              item
+              style={{ padding: 20 }}
+              xs={6}
+              container
+              alignItems="center"
+              justifyContent="center"
+            >
+              <IconButton
+                imageUrl={PlaceholderRotated}
+                name={"Exames Realizados"}
+              />
+            </Grid>
+            <Grid
+              item
+              style={{ padding: 20 }}
+              xs={6}
+              container
+              alignItems="center"
+              justifyContent="center"
+            >
+              <IconButton imageUrl={Sheet} name={"Hábitos & Saúde"} />
+            </Grid>
+            <Grid
+              item
+              style={{ padding: 20 }}
+              xs={6}
+              container
+              alignItems="center"
+              justifyContent="center"
+            >
+              <IconButton
+                imageUrl={MedicalResult}
+                name={"Questionário de Emergência"}
+              />
             </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Container>
+    </Grid>
   );
 };
 
--- frontend/src/views/Home/index.jsx
+++ frontend/src/views/Home/index.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import HomeComponent from "components/Home";
 import HomeBackground from "assets/home-background.png";
-import Header from 'components/Header'
+import Header from "components/Header";
 import FloatingUserInformation from "components/FloatingUserInformation";
+import { Grid } from "@material-ui/core";
 
 const Home = () => {
   return (
-    <div>
-      <Header backgroundImage={HomeBackground} />
-      <FloatingUserInformation />
-      <HomeComponent />
-    </div>
+    <Grid container direction="column" style={{ height: "100vh" }}>
+      <Grid item>
+        <Header backgroundImage={HomeBackground} />
+        <FloatingUserInformation />
+      </Grid>
+      <Grid style={{ flexGrow: 1 }} item xs>
+        <HomeComponent />
+      </Grid>
+    </Grid>
   );
 };
 
