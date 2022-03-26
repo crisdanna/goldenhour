0b33a9efd6562121f51b87de9fdd88891c4c3437
Richardson William <richardsonwtr@gmail.com>
2022-03-26 16:15:25 -0300

    feat: Adding the router

---
+++ frontend/src/router/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Switch, BrowserRouter } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
