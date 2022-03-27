e9fd43d65b1de20fbd3e214a3fbd09c4c61ae3af
Richardson William <richardsonwtr@gmail.com>
2022-03-27 10:22:32 -0300

    feat: Adding routes for the questionnaires

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Home from "views/Home";
+import HabitsAndHealth from "views/HabitsAndHealth";
+import EmergencyQuestionnaire from "views/EmergencyQuestionnaire";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/emergency-questionnaire" component={EmergencyQuestionnaire} />
+        <Route exact path="/habits-and-health" component={HabitsAndHealth} />
       </Switch>
     </BrowserRouter>
   );
