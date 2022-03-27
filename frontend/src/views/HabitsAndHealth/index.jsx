83fa3dce2d44668f3b482dbac985e03d6d0f015d
Richardson William <richardsonwtr@gmail.com>
2022-03-27 09:45:32 -0300

    mais uma parte

--- frontend/src/views/HabitsAndHealth/index.jsx
+++ frontend/src/views/HabitsAndHealth/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import {
+  Button,
+  Container,
   Grid,
   FormControl,
   FormLabel,
   FormControlLabel,
   Radio,
   RadioGroup,
+  Typography,
 } from "@material-ui/core";
 import HealthImage from "assets/habits-and-health.png";
 import Header from "components/Header";
@@ -61,6 +64,9 @@ const HabitsAndHealth = () => {
       alternatives: ["SIM", "NÃO"],
     },
   ];
+  const title = "Hábitos & Saúde";
+  const buttonText = "Atualizar informações";
+
   return (
     <Grid container direction="column" style={{ minHeight: "100vh" }}>
       <Grid item>
@@ -68,57 +74,79 @@ const HabitsAndHealth = () => {
       </Grid>
       <Grid item xs style={{ flexGrow: 1 }}>
         {/* QuestionnaireScreen */}
-
         <Grid item container direction="column" style={{ height: "100%" }}>
-          <Grid item style={{ backgroundColor: "gold" }}>
-            <div>Hábitos & Saúde</div>
-            <div>
-              *Este questionário tem o objetivo de acompanhar os seus hábitos de
-              saúde diários, procure sempre mantê-lo atualizado!
-            </div>
+          <Grid item style={{ marginTop: 10, marginBottom: 10 }}>
+            <Container>
+              <Typography style={{ color: "#0165B6" }}>{title}</Typography>
+              <Typography
+                variant="subtitle1"
+                style={{ fontStyle: "italic", fontSize: 10 }}
+              >
+                *Este questionário tem o objetivo de acompanhar os seus hábitos
+                de saúde diários, procure sempre mantê-lo atualizado!
+              </Typography>
+            </Container>
           </Grid>
-          <Grid item xs style={{ backgroundColor: "green" }}>
-            <Grid
-              item
-              container
-              direction="column"
-              style={{ backgroundColor: "#babaca" }}
-            >
-              {questions.map((question) => (
-                <Grid item xs key={question.title}>
-                  <FormControl component="fieldset" fullWidth>
-                    <FormLabel component="legend">{question.title}</FormLabel>
-                    <RadioGroup
-                      row
-                      //aria-label="gender"
-                      name={question.title}
-                      //value={value}
-                      //onChange={handleChange}
-                    >
-                      <Grid
-                        item
-                        container
-                        style={{ backgroundColor: "#babaca" }}
+          <Grid item xs style={{}}>
+            <Grid item container direction="column" style={{}}>
+              <Container>
+                {questions.map((question) => (
+                  <Grid item xs key={question.title} style={{ marginTop: 15 }}>
+                    <FormControl component="fieldset" fullWidth>
+                      <FormLabel
+                        component="legend"
+                        style={{ fontSize: 14, color: "black" }}
                       >
-                        {question.alternatives.map((alternative) => (
-                          <Grid item xs={6} style={{ backgroundColor: "blue" }}>
-                            <FormControlLabel
-                              key={alternative}
-                              value={alternative}
-                              label={alternative}
-                              control={<Radio />}
-                            />
-                          </Grid>
-                        ))}
-                      </Grid>
-                    </RadioGroup>
-                  </FormControl>
-                </Grid>
-              ))}
+                        {question.title}
+                      </FormLabel>
+                      <RadioGroup
+                        row
+                        //aria-label="gender"
+                        name={question.title}
+                        //value={value}
+                        //onChange={handleChange}
+                      >
+                        <Grid
+                          item
+                          container
+                          style={{ marginLeft: 20, marginRight: 20 }}
+                        >
+                          {question.alternatives.map((alternative) => (
+                            <Grid item xs={6} style={{}}>
+                              <FormControlLabel
+                                key={alternative}
+                                value={alternative}
+                                label={
+                                  <Typography
+                                    style={{ fontSize: 12, color: "#424244" }}
+                                  >
+                                    {alternative}
+                                  </Typography>
+                                }
+                                control={<Radio />}
+                              />
+                            </Grid>
+                          ))}
+                        </Grid>
+                      </RadioGroup>
+                    </FormControl>
+                  </Grid>
+                ))}
+              </Container>
             </Grid>
           </Grid>
-          <Grid item style={{ backgroundColor: "darkred" }}>
-            botao
+          <Grid item style={{}}>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{
+                width: "100%",
+                height: "100%",
+                backgroundColor: "#0165B6",
+              }}
+            >
+              {buttonText}
+            </Button>
           </Grid>
         </Grid>
       </Grid>
